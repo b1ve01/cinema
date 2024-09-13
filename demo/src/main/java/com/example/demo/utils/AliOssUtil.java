1c356f4d3f923b6ed1d895b1fb8636f8ea8373f3
b1ve01 <1203610735@qq.com>
2024-09-13 17:22:16 +0800

    .

--- demo/src/main/java/com/example/demo/utils/AliOssUtil.java
+++ demo/src/main/java/com/example/demo/utils/AliOssUtil.java
@@ -16,8 +16,8 @@ public class AliOssUtil {
     private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5tRs6xKQi6BFyCLenvyq";
-    private static final String ACCESS_KEY_SECRET="lhWblyOLfWw5BBbaRaOPlyV4kcCBVE";
+    private static final String ACCESS_KEY_ID="******";
+    private static final String ACCESS_KEY_SECRET="******";
     // 填写Bucket名称，例如examplebucket。
     private static final String BUCKET_NAME = "cinema-bucket";
 
